4788f1d276c90d2df018aaacf6e99f682f9cbb7e
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2025-03-11 20:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your code here.
+const items = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+items.addEventListener('mousedown', (e) => {
+  isDown = true;
+  items.classList.add('active');
+
+  // Get the initial mouse position and scroll position
+  startX = e.pageX - items.offsetLeft;
+  scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+  isDown = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mouseup', () => {
+  isDown = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+  if (!isDown) return; // Stop if mouse button is not pressed
+  e.preventDefault();
+
+  // Calculate new position based on mouse movement
+  const x = e.pageX - items.offsetLeft;
+  const walk = (x - startX) * 2; // Adjust sensitivity by multiplying
+
+  // Update scroll position
+  items.scrollLeft = scrollLeft - walk;
+});
